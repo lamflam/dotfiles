efd56402cef33b54fc4226923ad2c864529f4b79
Kevin LaFlamme <k@lamfl.am>
2020-12-18 15:19:09 -0500

    Add search/replace alias agr

--- aliases.sh
+++ aliases.sh
@@ -9,3 +9,7 @@ alias ls='ls -alh'
 alias ag='ag -p $DOTFILES/ag/.ignore'
 
 alias sed='gsed'
+
+function agr() {
+    ag -0 -l "$1" | AGR_FROM="$1" AGR_TO="$2" xargs -0 perl -pi -e 's/$ENV{AGR_FROM}/$ENV{AGR_TO}/g';
+}
